0866ded3cdcd697ed98cb85a5833498a7c498d10
IagoSiccardi <iago.siccardi@gmail.com>
2022-05-03 21:42:38 -0300

    Random Products

--- Sunset/src/controllers/productController.js
+++ Sunset/src/controllers/productController.js
@@ -6,6 +6,24 @@ const getRandom = (min, max) => {
     return Math.round( Math.random() * (max - min) + min)
 
   }
+  
+  const resultado = (getRandom(1,products.length))
+  const resultado2 = (getRandom(1,products.length))
+  const resultado3 = (getRandom(1,products.length))
+  let result = []
+  
+for (let i = 0; i < 1; i++) {
+
+
+    result.push(resultado)
+    result.push(resultado2)
+    result.push(resultado3)
+
+}
+
+console.log(result)
+
+
 
 
   
@@ -23,7 +41,8 @@ module.exports = {
         
         res.render ('./products/productDetail',{
             product,
-            getRandom
+            products,
+            result
 
         })
     },
